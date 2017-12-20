1517128018e041927e308f76947eef31f401938c
Jarven <769558579@qq.com>
2017-12-20 16:34:07 +0800

    订单列表接口

--- src/main/java/com/kunlun/api/controller/OrderController.java
+++ src/main/java/com/kunlun/api/controller/OrderController.java
@@ -1,12 +1,19 @@
 package com.kunlun.api.controller;
 
 import com.kunlun.api.service.ROrderService;
+import com.kunlun.result.DataRet;
 import com.kunlun.result.PageResult;
+import com.kunlun.wxentity.OrderCondition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * @author by kunlun
@@ -21,8 +28,44 @@ public class OrderController {
     @Autowired
     private ROrderService rOrderService;
 
+
+    /**
+     * 订单列表
+     *
+     * @param orderNo   订单编号
+     * @param phone     收货人手机号
+     * @param status    订单状态
+     * @param type      订单类型
+     * @param searchKey 搜索关键字
+     * @param pageNo    当前页
+     * @param pageSize  每页条数
+     * @return
+     */
     @GetMapping("/list")
-    public PageResult list(@RequestParam Integer pageNo, @RequestParam Integer pageSize) {
-        return rOrderService.list(pageNo, pageSize);
+    public PageResult list(@RequestParam(value = "order_no") String orderNo,
+                           @RequestParam(value = "phone") String phone,
+                           @RequestParam(value = "status") String status,
+                           @RequestParam(value = "type") String type,
+                           @RequestParam(value = "search_key") String searchKey,
+                           @RequestParam(value = "page_no") Integer pageNo,
+                           @RequestParam(value = "page_size") Integer pageSize) {
+        return rOrderService.list(orderNo, phone, status, type, searchKey,pageNo,pageSize);
+    }
+
+    /**
+     * 发货
+     * orderId
+     * deliveryNo
+     * expressName
+     * expressNo
+     * senderId
+     *
+     * @param orderCondition
+     * @return
+     */
+    @PostMapping("/sendGood")
+    public DataRet<String> sendGood(@RequestBody OrderCondition orderCondition) {
+
+        return null;
     }
 }
--- src/main/java/com/kunlun/api/service/ROrderService.java
+++ src/main/java/com/kunlun/api/service/ROrderService.java
@@ -2,8 +2,10 @@ package com.kunlun.api.service;
 
 import com.kunlun.api.hystrix.ROrderServiceHystrix;
 import com.kunlun.result.PageResult;
+import com.kunlun.wxentity.OrderCondition;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -14,7 +16,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "cloud-service-order", fallback = ROrderServiceHystrix.class)
 public interface ROrderService {
 
+    /**
+     * @param orderNo   订单编号
+     * @param phone     收货人手机号
+     * @param status    订单状态
+     * @param type      订单类型
+     * @param searchKey 搜索关键字
+     * @param pageNo    当前页
+     * @param pageSize  每页条数
+     * @return
+     */
     @GetMapping("/base/order/list")
-    PageResult list(@RequestParam(value = "pageNo") Integer pageNo, @RequestParam(value = "pageSize") Integer pageSize);
+    PageResult list(@RequestParam(value = "order_no") String orderNo,
+                    @RequestParam(value = "phone") String phone,
+                    @RequestParam(value = "status") String status,
+                    @RequestParam(value = "type") String type,
+                    @RequestParam(value = "search_key") String searchKey,
+                    @RequestParam(value = "page_no") Integer pageNo,
+                    @RequestParam(value = "page_size") Integer pageSize);
 
 }
