888fb4a6bae02b5991e730ad7f4ebaf4d23116e2
Jarven <769558579@qq.com>
2017-12-20 14:14:56 +0800

    订单接口

--- src/main/java/com/kunlun/api/controller/WxOrderController.java
+++ src/main/java/com/kunlun/api/controller/WxOrderController.java
@@ -1,12 +1,16 @@
 package com.kunlun.api.controller;
 
 import com.kunlun.api.service.WxOrderService;
+import com.kunlun.entity.OrderExt;
+import com.kunlun.result.DataRet;
 import com.kunlun.result.PageResult;
 import com.kunlun.wxentity.OrderCondition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -34,5 +38,40 @@ public class WxOrderController {
         return wxOrderService.findByCondition(orderCondition);
     }
 
+    /**
+     * 退款
+     *
+     * @return
+     */
+    @PostMapping("/refund")
+    public DataRet<String> refund() {
+        return null;
+    }
+
+    /**
+     * 查询订单详情
+     *
+     * @param orderId
+     * @return
+     */
+    @GetMapping("/findById")
+    public DataRet<OrderExt> findById(@RequestParam(value = "orderId") Long orderId) {
+        return null;
+    }
+
+    @PostMapping("/estimate")
+    public DataRet<String> estimate() {
+        return null;
+    }
+
+    /**
+     * 确认收货
+     *
+     * @return
+     */
+    @PostMapping("/confirmReceive")
+    public DataRet<String> confirmReceive() {
+        return null;
+    }
 
 }
