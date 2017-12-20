329a4a943c1a44418772a3a85f69e2acc1fe337f
Jarven <769558579@qq.com>
2017-12-20 16:00:05 +0800

    断路器监控添加到注册中心

--- src/main/java/com/kunlun/CloudRibbonServerApplication.java
+++ src/main/java/com/kunlun/CloudRibbonServerApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableHystrix
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class CloudRibbonServerApplication {
 
