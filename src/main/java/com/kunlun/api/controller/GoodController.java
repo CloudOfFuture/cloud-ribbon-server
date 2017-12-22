65420d909787e4ed9d0602f1aa9b5378968757d2
MoonHong <1049313183@qq.com>
2017-12-22 15:09:51 +0800

    分页查询

--- src/main/java/com/kunlun/api/controller/GoodController.java
+++ src/main/java/com/kunlun/api/controller/GoodController.java
@@ -3,8 +3,11 @@ package com.kunlun.api.controller;
 import com.kunlun.api.service.GoodService;
 import com.kunlun.entity.Good;
 import com.kunlun.result.DataRet;
+import com.kunlun.result.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 /**
  * @author by hmy
@@ -39,5 +42,40 @@ public class GoodController {
     @GetMapping("/findById")
     public DataRet<Good> findById(@RequestParam(value = "id") Long id) {
         return goodService.findById(id);
+    }
+
+
+    /**
+     * 分页查询
+     *
+     * @param pageNo
+     * @param pageSize
+     * @param searchKey
+     * @param goodNo
+     * @param startDate
+     * @param endDate
+     * @param brandId
+     * @param onSale     上下架 上下ON_SALE", "上架 OFF_SALE", "下架")
+     * @param categoryId
+     * @param hot        热销 是否热销 HOT", "热销 NOT_HOT", "非热销
+     * @param isNew      新品 是否新品 IS_NEW", "新品 NOT_NEW", "非新品
+     * @param freight    包邮 是否包邮 FREIGHT包邮 UN_FREIGHT 不包邮
+     * @return
+     */
+    @GetMapping("/findByCondition")
+    public PageResult findByCondition(@RequestParam(value = "page_no") Integer pageNo,
+                                      @RequestParam(value = "page_size") Integer pageSize,
+                                      @RequestParam(value = "search_key",required = false) String searchKey,
+                                      @RequestParam(value = "good_no",required = false) String goodNo,
+                                      @RequestParam(value = "start_date",required = false) Date startDate,
+                                      @RequestParam(value = "end_date",required = false) Date endDate,
+                                      @RequestParam(value = "brand_id",required = false) Long brandId,
+                                      @RequestParam(value = "on_sale",required = false) String onSale,
+                                      @RequestParam(value = "category_id",required = false) Long categoryId,
+                                      @RequestParam(value = "hot",required = false) String hot,
+                                      @RequestParam(value = "is_new",required = false) String isNew,
+                                      @RequestParam(value = "freight",required = false) String freight) {
+        return goodService.findByCondition(pageNo, pageSize, searchKey, goodNo, startDate, endDate,
+                brandId, onSale, categoryId, hot, isNew, freight);
     }
 }
--- src/main/java/com/kunlun/api/service/GoodService.java
+++ src/main/java/com/kunlun/api/service/GoodService.java
@@ -3,11 +3,14 @@ package com.kunlun.api.service;
 import com.kunlun.api.hystrix.GoodServiceHystrix;
 import com.kunlun.entity.Good;
 import com.kunlun.result.DataRet;
+import com.kunlun.result.PageResult;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 /**
  * @author by hmy
@@ -35,4 +38,35 @@ public interface GoodService {
      */
     @GetMapping("/backstage/good/findById")
     DataRet<Good> findById(@RequestParam(value = "id") Long id);
+
+    /**
+     * 分页查询
+     *
+     * @param pageNo
+     * @param pageSize
+     * @param searchKey
+     * @param goodNo
+     * @param startDate
+     * @param endDate
+     * @param brandId
+     * @param onSale
+     * @param categoryId
+     * @param hot
+     * @param isNew
+     * @param freight
+     * @return
+     */
+    @GetMapping("/backstage/good/findByCondition")
+    PageResult findByCondition(@RequestParam(value = "page_no") Integer pageNo,
+                               @RequestParam(value = "page_size") Integer pageSize,
+                               @RequestParam(value = "search_key",required = false) String searchKey,
+                               @RequestParam(value = "good_no",required = false) String goodNo,
+                               @RequestParam(value = "start_date",required = false) Date startDate,
+                               @RequestParam(value = "end_date",required = false) Date endDate,
+                               @RequestParam(value = "brand_id",required = false) Long brandId,
+                               @RequestParam(value = "on_sale",required = false) String onSale,
+                               @RequestParam(value = "category_id",required = false) Long categoryId,
+                               @RequestParam(value = "hot",required = false) String hot,
+                               @RequestParam(value = "is_new",required = false) String isNew,
+                               @RequestParam(value = "freight",required = false) String freight);
 }
