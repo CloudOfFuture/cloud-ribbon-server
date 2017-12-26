b05bef165a08371e28e69e5a1248d21ed0c91c31
HWS <15757135983@163.com>
2017-12-26 09:27:50 +0800

    1.ribbon中收货地址

--- src/main/java/com/kunlun/api/client/DeliveryClient.java
+++ src/main/java/com/kunlun/api/client/DeliveryClient.java
@@ -1,7 +1,6 @@
-package com.kunlun.api.service;
+package com.kunlun.api.client;
 
-
-import com.kunlun.api.hystrix.DeliveryServiceHystrix;
+import com.kunlun.api.hystrix.DeliveryClientHystrix;
 import com.kunlun.result.DataRet;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author by hws
  * @created on 2017/12/25.
  */
-public interface DeliveryService {
+@FeignClient(value = "cloud-service-user-center", fallback = DeliveryClientHystrix.class)
+public interface DeliveryClient {
 
     /**
      * 根据id查找收获地址详情
      * @param id
      * @return
      */
-    DataRet findById(Long id);
-
+    @GetMapping("/delivery/findDetailById")
+    DataRet findById(@RequestParam(value = "id") Long id);
 }
---
+++ src/main/java/com/kunlun/api/hystrix/DeliveryClientHystrix.java
@@ -0,0 +1,24 @@
+package com.kunlun.api.hystrix;
+
+import com.kunlun.api.client.DeliveryClient;
+import com.kunlun.result.DataRet;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author by hws
+ * @created on 2017/12/25.
+ */
+@Component
+public class DeliveryClientHystrix implements DeliveryClient{
+
+    /**
+     * 根据id查找收获地址详情
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public DataRet findById(Long id) {
+        return new DataRet("Error","收获地址无效");
+    }
+}
