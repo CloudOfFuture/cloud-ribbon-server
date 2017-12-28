4ab212a31466d9317863b0f6e1a6d725d8042f3b
JackSpeed <1062789415@qq.com>
2017-12-28 10:35:29 +0800

    用户修改认真信息  图片信息保存完成

--- src/main/java/com/kunlun/config/FeignMultipartConfig.java
+++ src/main/java/com/kunlun/config/FeignMultipartConfig.java
@@ -3,9 +3,6 @@ package com.kunlun.config;
 import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
 
 /**
  * @author JackSpeed
